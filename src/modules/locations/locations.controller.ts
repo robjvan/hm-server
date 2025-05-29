ef480df4dc44b4c2e83ee21f7aad839c98aef633
Rob Vandelinder <robjvan@gmail.com>
2025-05-29 19:29:50 -0230

    Added record fetching

--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -1,7 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { LocationsService } from './locations.service';
 
 @Controller('locations')
 export class LocationsController {
   constructor(private readonly locationsService: LocationsService) {}
+
+  @Get('/report')
+  getReport() {}
+
+  // @Get('/:id')
+  // fetchOneById(@Param('id') id: number) {
+  //   return this.locationsService.fetchRecordById('location', id);
+  // }
+
+  // @Get('/city/:id')
+  // fetchCityById(@Param('id') id: number) {
+  //   return this.locationsService.fetchRecordById('city', id);
+  // }
+
+  // @Get('/country/:id')
+  // fetchCountryById(@Param('id') id: number) {
+  //   return this.locationsService.fetchRecordById('country', id);
+  // }
+
+  // @Get('/region/:id')
+  // fetchRegionById(@Param('id') id: number) {
+  //   return this.locationsService.fetchRecordById('region', id);
+  // }
+
+  @Get('/user/:userId')
+  fetchOneByUserId(@Param('userId') userId: number) {
+    return this.locationsService.fetchOneByUserId(userId);
+  }
+
+  @Post()
+  addNewRecord(@Body() data: any) {
+    console.log(data);
+  }
+
+  @Patch('/:id')
+  updateOneById(@Param('id') id: number, @Body() data: any) {
+    console.log(id, data);
+  }
+
+  @Delete('/:id')
+  deleteOneById(@Param('id') id: number) {
+    console.log(id);
+  }
 }
