67f63b123980aab8cdd48c68857914b0649332f2
Rob Vandelinder <robjvan@gmail.com>
2025-05-29 19:29:40 -0230

    Added starter endpoints

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/:id')
+  fetchOneById(@Param('id') id: number) {
+    console.log(id);
+  }
+
+  @Patch('/:id')
+  updateOneById(@Param('id') id: number, @Body() data: any) {
+    console.log(id, data);
+  }
+
+  @Delete('/:id')
+  deleteOneById(@Param('id') id: number) {
+    console.log(id);
+  }
 }
